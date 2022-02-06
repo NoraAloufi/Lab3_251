128eb8b4f06da78e9f9123b62faad6de5e764c06
NoraAloufi <98253252+NoraAloufi@users.noreply.github.com>
2022-02-06 10:55:27 +0300

    I wrote some coding stuff :D

--- 251Lab3/src/pkg251lab3/Main.java
+++ 251Lab3/src/pkg251lab3/Main.java
@@ -16,7 +16,7 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        system.out.println("Trying to pull the code");
+        system.out.println("Trying to pull the code ");
          
     }
     
